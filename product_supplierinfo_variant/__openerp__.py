36de7d75edca7fac168a2adee0d3e2f35f19b3fc
florent.thomas <florent.thomas@mind-and-go.com>
2015-11-24 08:49:23 +0100

    Some part of the code are no more necessary Improve views and context so that it will be esier to deal with pricelist

--- product_supplierinfo_variant/__openerp__.py
+++ product_supplierinfo_variant/__openerp__.py
@@ -25,6 +25,7 @@
     ],
     'data': [
         'views/product_template_view.xml',
+        'views/product_supplier_info.xml',
     ],
     'installable': True,
     "post_init_hook": "duplicate_supplierinfo_per_variant",
